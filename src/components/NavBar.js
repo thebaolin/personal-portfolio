9d85830752c3376b71fea2b29efcfe98238f939e
thebaolin <bchang916@gmail.com>
2022-10-09 18:33:08 -0400

    fixed errors with navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,10 +21,9 @@ export const NavBar = () => {
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
 
-    //hello
-asdasdad
+    
     return {
-        <Navbar expand="lg">
+        <Navbar expand="lg" className= 9scrolled > "scrolled": "")>
         <Container>
             <Navbar.Brand href="#home">
             <img src={''} alt="Logo" />
@@ -35,9 +34,9 @@ asdasdad
 
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#skills">Skills</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link href="#home" className= {activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                <Nav.Link href="#skills" className= {activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick = {() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className= {activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick = {() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                 
             </Nav>
             <span className="navbar-text">
